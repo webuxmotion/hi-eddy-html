d20cca5df7be5434c6d26a8ee3f595f33492d7ef
Andrii Pereverziev <pereverziev.andrii@gmail.com>
2022-06-22 23:25:13 +0300

    Pages

--- js/helpers.js
+++ js/helpers.js
@@ -1,4 +1,5 @@
 insertNavigationOnTop();
+layout();
 loadIconsScript();
 loadStyles(() => {
   document.body.style.opacity = 1;
@@ -43,6 +44,21 @@ function insertNavigationOnTop() {
       </a>
     </div>
     <div class="nav-item">
+      <div class="nav-item__dropdown">
+        <ul>
+          <li>
+            <a href="./pages/home/home.html">home</a>
+          </li>
+          <li>
+            <a href="./pages/example/example.html">example</a>
+          </li>
+        </ul>
+      </div>
+      <a class="nav-item__control">
+        <span class="nav-item__text">Pages</span>
+      </a>
+    </div>
+    <div class="nav-item">
       <a 
         target="_blank"
         href="https://www.figma.com/file/ADcMvwBp6S51uSI4Xlkt23/hi-20jun-2022-tariff-cards-ui?node-id=0%3A1" 
@@ -84,6 +100,8 @@ function loadStyles(callback) {
     './components/header/header.css',
     './components/sidebar/sidebar.css',
     './components/price-card/price-card.css',
+    './pages/home/home.css',
+    './pages/example/example.css',
   ];
 
   links.forEach((oneLink) => {
@@ -97,4 +115,142 @@ function loadStyles(callback) {
   setTimeout(() => {
     callback();
   }, 100);
+}
+
+function layout() {
+  const pageElement = document.getElementById('page');
+  if (pageElement) {
+    const pageContent = pageElement.innerHTML;
+    const fullPageCode = getCode(pageContent);
+    pageElement.innerHTML = fullPageCode;
+  }
+  function getCode(content) {
+    return `
+    <div class="l-layout">
+    <aside class="l-layout__sidebar">
+      ${sidebarVariant1()}
+    </aside>
+    <div class="l-layout__main">
+      <div class="l-layout__header">
+        ${headerVariant1()}
+      </div>
+      <div class="l-layout__content">
+        ${content}
+      </div>
+    </div>
+  </div>
+    `;
+  }
+}
+
+function headerVariant1() {
+  return `
+  <!-- header -->
+  <header class="header">
+    <div class="header__navigation">
+      <!-- nav-item -->
+      <div class="nav-item" tabindex="0">
+        <div class="nav-item__dropdown">
+          <ul>
+            <li>
+              <a href="#">про академію</a>
+            </li>
+            <li>
+              <a href="#">політика конфіденційності</a>
+            </li>
+            <li>
+              <a href="#">оферта</a>
+            </li>
+            <li>
+              <a href="#">оплата та доставка</a>
+            </li>
+          </ul>
+        </div>
+        <a class="nav-item__control">
+          <span class="nav-item__text">Про нас</span>
+        </a>
+      </div>
+      <!-- END. nav-item -->
+      <!-- nav-item -->
+      <div class="nav-item">
+        <a href="#" class="nav-item__control">
+          <span class="nav-item__text">Ціни</span>
+        </a>
+      </div>
+      <!-- END. nav-item -->
+    </div>
+    <!-- button -->
+    <a href="#" class="button button--header button--header-bottom-radius">
+      <span class="button__icon" data-icon="user"></span>
+      Вхід</a
+    >
+    <!-- END. button -->
+    <div class="header__corner-button">
+      <!-- button -->
+      <a href="#" class="button button--header button--header-corner">
+        РЕЄстрація
+      </a>
+      <!-- END. button -->
+    </div>
+  </header>
+  <!-- END. header -->
+  `;
+}
+
+function sidebarVariant1() {
+  return `
+  <!-- sidebar -->
+  <div class="sidebar">
+    <!-- logo -->
+    <a href="#" class="logo">
+      <span class="logo__image" data-icon="logo"></span>
+      <span class="logo__text">hi-eddy</span>
+    </a>
+    <!-- END. logo -->
+    <div class="sidebar__group">
+      <!-- sidebar-button -->
+      <a href="" class="sidebar-button">
+        <span class="sidebar-button__icon" data-icon="lessons"></span>
+        <span class="sidebar-button__text">Уроки</span>
+      </a>
+      <!-- END. sidebar-button -->
+      <!-- sidebar-button -->
+      <a href="#" class="sidebar-button">
+        <span class="sidebar-button__icon" data-icon="courses"></span>
+        <span class="sidebar-button__text">Курси</span>
+      </a>
+      <!-- END. sidebar-button -->
+    </div>
+    <div class="sidebar__divider"></div>
+    <div class="sidebar__group">
+      <!-- sidebar-button -->
+      <a href="#" class="sidebar-button">
+        <span class="sidebar-button__icon" data-icon="saved"></span>
+        <span class="sidebar-button__text">
+          <span class="sidebar-button__count">22</span>
+          Збережені
+        </span>
+      </a>
+      <!-- END. sidebar-button -->
+      <!-- sidebar-button -->
+      <a href="#" class="sidebar-button">
+        <span class="sidebar-button__icon" data-icon="paw"></span>
+        <span class="sidebar-button__text">Завершені</span>
+      </a>
+      <!-- END. sidebar-button -->
+      <div class="sidebar__button-wrapper">
+        <!-- button -->
+        <a
+          href="#"
+          class="button button--bordered button--fullwidth button--border-radius"
+        >
+          <span class="button__icon" data-icon="ua-flag"></span>
+          Допомога ЗСУ
+        </a>
+        <!-- END. button -->
+      </div>
+    </div>
+  </div>
+  <!-- END. sidebar -->
+  `;
 }
