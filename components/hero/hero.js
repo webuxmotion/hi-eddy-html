ad94ec0690b3bf4a3ab4879b0fe8a95e64c60991
Andrii Pereverziev <pereverziev.andrii@gmail.com>
2022-06-23 12:10:13 +0300

    button wrapper

--- components/hero/hero.js
+++ components/hero/hero.js
@@ -6,9 +6,13 @@ function heroVariant1(attr) {
         <div class="hero__main">
           <h1 class="hero__title">Академія hi-eddy <br />вітає вас</h1>
           <h2 class="hero__subtitle">В нас є 2 курси з програмування</h2>
-          <!-- button -->
-          <a class="button button--border-radius">Дивитись курси</a>
-          <!-- END. button -->
+          <div class="hero__button-wrapper">
+            <!-- button -->
+            <a class="button button--border-radius button--fullwidth">
+              Дивитись курси
+            </a>
+            <!-- END. button -->
+          </div>
 
           <div class="hero__why">
             <h3 class="hero__why-title">Чому люди обрають нас?</h3>
