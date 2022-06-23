244ce1960aa2e76c15485b64a054a0aa0026ab02
Andrii Pereverziev <pereverziev.andrii@gmail.com>
2022-06-23 12:01:01 +0300

    hero

--- components/hero/hero.js
+++ components/hero/hero.js
@@ -2,7 +2,29 @@ function heroVariant1(attr) {
   return `
     <!-- hero -->
     <div class="hero" data-name="${attr}">
-      hero
+      <div class="hero__column hero__column--main">
+        <div class="hero__main">
+          <h1 class="hero__title">Академія hi-eddy <br />вітає вас</h1>
+          <h2 class="hero__subtitle">В нас є 2 курси з програмування</h2>
+          <!-- button -->
+          <a class="button button--border-radius">Дивитись курси</a>
+          <!-- END. button -->
+
+          <div class="hero__why">
+            <h3 class="hero__why-title">Чому люди обрають нас?</h3>
+            <p class="hero__why-text">
+              Нас обирають, тому що ми поважаємо ваш час. Наприклад, наші курси
+              дають вам знання, за допомогою яких ви зможете безкінечно
+              наповнювати своє портфоліо якісними роботами.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="hero__column">
+        <div class="hero__image-wrapper">
+          <img class="hero__image" src="./img/eddy.svg" alt="" />
+        </div>
+      </div>
     </div>
     <!-- END. hero -->
   `;
