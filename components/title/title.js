4113486ff09e8cb4c402ba401d2e0f329423dccf
Andrii Pereverziev <pereverziev.andrii@gmail.com>
2022-06-25 14:41:32 +0300

    upgrade title

--- components/title/title.js
+++ components/title/title.js
@@ -1,15 +1,16 @@
 function getTitleParamsKeys() {
-  return ['title', 'tag'];
+  return ['title', 'tag', 'classes'];
 }
 
 function getTitle(params) {
-  const { title, tag } = extractParams(params, getTitleParamsKeys());
+  const { title, tag, classes } = extractParams(params, getTitleParamsKeys());
 
   const newTag = tag === 'test-tag' ? 'h1' : tag;
+  const additionalClasses = classes === 'test-classes' ? '' : classes;
 
   return `
     <!-- title -->
-    <${newTag} class="title" data-name="${params.component}">
+    <${newTag} class="title ${additionalClasses}" data-name="${params.component}">
       ${title}
     </${newTag}>
     <!-- END. title -->
